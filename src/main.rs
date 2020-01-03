4c4552a709f8e989eb575e64eb5a851a5d2533c7
root <root@vultr.guest>
2020-01-03 13:52:16 +0000

    request animation frame not working yet

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 use web_sys;
 
 fn main() {
-    animation();
+    let window = web_sys::window().expect("no window found");
+    let animation = |window: &web_sys::Window| {
+        window.request_animation_frame(&animation);
+    };
+
+    animation(&window);
     println!("Hello, world!");
 }
 
-fn animation(){
-    web_sys::window().expect("no window found").request_animation_frame(&animation);
-}
